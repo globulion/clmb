d9c71e6321f853887dbf4b59876aabc7d0c57079
Robert Góra <robert.gora@pwr.edu.pl>
2014-11-30 18:32:06 +0100

    Version update in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='Coulomb',
-      version='1.0.1',
+      version='1.0.2',
       description='Coulomb interaction energy calculator',
       author='Bartosz Błasiak & R. W. Góra',
       author_email='globula@o2.pl',

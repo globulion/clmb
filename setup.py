b1c8c19cfbde985d0ad6fbe65c773c0dc654fe98
globulion <blasiak.bartosz@gmail.com>
2015-04-20 23:11:31 +0900

    Revision

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 from distutils.core import setup
 
 setup(name='Coulomb',
-      version='1.0.2',
+      version='1.0.3',
       description='Coulomb interaction energy calculator',
       author='Bartosz Błasiak & R. W. Góra',
-      author_email='globula@o2.pl',
+      author_email='blasiak.bartosz@gmail.com',
       url='http://www.ex.no/pymod/m1',
       packages=['coulomb'],
      )
